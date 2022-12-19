e512c025cae62f308878e444f97b2aba128321d6
mhrifai <mkhrifai99@gmail.com>
2022-12-19 08:10:33 +0700

    update

---
+++ src/queueCollection/QueueCollection.java
@@ -0,0 +1,13 @@
+package queueCollection;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueCollection {
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayDeque<>(10);
+        queue.offer("Rifai");
+
+
+    }
+}
